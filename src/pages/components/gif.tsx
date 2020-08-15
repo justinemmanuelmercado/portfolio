f3837f7ab122a9157fdec5dff9bbe4c6062f8525
EJ Mercado <justinemmanuelmercado@gmail.com>
2020-08-15 08:00:10 +0800

    add image loading and preloading

--- src/pages/components/gif.tsx
+++ src/pages/components/gif.tsx
@@ -1,16 +1,21 @@
 import React, { useState } from 'react';
-import { FaPlayCircle } from 'react-icons/fa';
+import { FaPlayCircle, FaSpinner } from 'react-icons/fa';
 
 const Gif = ({ gif, still }: {gif: string, still: string}) => {
   const [playing, setPlaying] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   return (
     <div className="gif-holder">
       {!playing && <div className="play-pause" onClick={() => setPlaying(true)}>
         <FaPlayCircle size="5rem"/>
       </div>}
-      <img className={`gif-still ${playing ? 'playing' : ''}`}loading="lazy" src={still}/>
-      <img className={'gif-gif'} onClick={() => setPlaying(false)} loading="lazy" src={playing ? gif : still} />
+      {loading && <div className="play-pause" onClick={() => setPlaying(true)}>
+        <FaSpinner size="5rem"/>
+      </div>}
+      <img className={`gif-still ${playing ? 'playing' : ''}`}loading="eager" src={still}/>
+      <link rel="preload" href={gif} as="image"/>
+      <img onLoad={() => setLoading(false)} className={'gif-gif'} onClick={() => setPlaying(false)} loading="lazy" src={playing ? gif : still} />
     </div>
   );
 };
